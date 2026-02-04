38160427248dd51bfe10b83c1324d8e46f54a368
belAlmendras <belen.almendros19@gmail.com>
2026-02-04 01:07:37 -0300

    first commit

---
+++ src/test/java/com/lluviadeideas/storiesapi/StoriesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lluviadeideas.storiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoriesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
