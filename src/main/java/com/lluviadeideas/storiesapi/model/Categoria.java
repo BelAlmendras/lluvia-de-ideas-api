f42987801a5190d80e2c3ef4d080ee227116f592
belAlmendras <belen.almendros19@gmail.com>
2026-02-07 21:44:19 -0300

    feat: add usuario and rol model

--- src/main/java/com/lluviadeideas/storiesapi/model/Categoria.java
+++ src/main/java/com/lluviadeideas/storiesapi/model/Categoria.java
@@ -1,19 +1,24 @@
-package com.lluviadeideas.storiesapi.entity;
+package com.lluviadeideas.storiesapi.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "categorias")
+@Getter @Setter
 public class Categoria {
     
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
-
 private Long id;
+
+@NotBlank(message = "El nombre de la categoria no debe estar vacio")
 private String nombre;
 
 public Categoria() {}
---
+++ src/main/java/com/lluviadeideas/storiesapi/model/Usuario.java
@@ -0,0 +1,92 @@
+package com.lluviadeideas.storiesapi.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table (name = "usuarios")
+@Getter @Setter
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "El nombre de usuario es obligatorio")
+    @Column(name = "nombre_usuario", unique = true, nullable = false)
+    @Size(min = 3, max = 20)
+    @Pattern(regexp = "^[a-zA-Z0-9_]+$")
+    private String nombreUsuario;
+
+    @NotBlank(message = "El nombre es obligatorio")
+    private String nombre;
+
+    @NotBlank(message = "El apellido es obligatorio")
+    private String apellido;
+
+    @NotNull(message = "La fecha de nacimiento es obligatoria")
+    @Column(name = "fecha_nacimiento")
+    private LocalDate fechaNacimiento;
+
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "El email debe ser valido")
+    @Column(unique = true, nullable = false)
+    private String correo;
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    @Size(min = 8, max = 30, message = "La contraseña debe tener al menos 8 caracteres. Maximo 30")
+    @Pattern(
+    regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).+$",
+    message = "La contraseña debe tener mayúscula, minúscula, número y símbolo")
+    @Column(nullable = false, length = 300)
+    private String password;
+
+    @Size(max = 500)
+    private String biografia;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Rol rol;
+
+    @Column(nullable = false)
+    private boolean activo = true;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+   @PrePersist
+   protected void onCreate() {
+    createdAt = LocalDateTime.now();
+    updatedAt = LocalDateTime.now();
+}
+
+   @PreUpdate
+   protected void onUpdate() {
+    updatedAt = LocalDateTime.now();
+}
+
+
+    public Usuario() {}
+
+
+}
